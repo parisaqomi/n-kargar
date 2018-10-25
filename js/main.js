9735a1f5382780092e70466c75179c8e579d9572
parisaqomi <parisaq97@gmail.com>
2018-10-25 18:17:49 +0330

    a tag

--- js/main.js
+++ js/main.js
@@ -89,3 +89,11 @@
 
 
 }());
+
+$('.btn').on('click', function() {
+    var $this = $(this);
+  $this.button('loading');
+    setTimeout(function() {
+       $this.button('reset');
+   }, 8000);
+});
